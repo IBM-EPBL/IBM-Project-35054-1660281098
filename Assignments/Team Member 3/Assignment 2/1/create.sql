27c2002b44ae80d2b0ed00fa14e6b1e05095ebef
SaiRam6425 <113855733+SaiRam6425@users.noreply.github.com>
2022-10-17 15:11:34 +0530

    uploaded

---
+++ Assignments/Team Member 3/Assignment 2/1/create.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "TXZ44264"."User Details" ( User CHAR(30) NOT NULL ,Roll INT NOT NULL ,Email CHAR(30) NOT NULL ,Password CHAR(30) NOT NULL )
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'monish',
+1,
+'monish@mail.com',
+'monish1'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'manigandan',
+2,
+'manigandan@mail.com',
+'manigandan2'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'rohith',
+3,
+'rohith@mail.com',
+'rohith3'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'sairam',
+4,
+'sairam@mail.com',
+'sairam4'
+);
+INSERT
+  INTO  "TXZ44264"."User Details" ("USER","ROLL","EMAIL","PASSWORD")
+  VALUES(
+'dummy',
+5,
+'dummy@mail.com',
+'dummy5'
+);
